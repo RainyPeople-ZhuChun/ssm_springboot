d1caaa8563711969ca6cf95d2e4af46f94ef7b29
zhuchun <zhuchun@rainypeople.com>
2020-11-24 19:00:56 +0800

    完成后台管理分页显示

--- tmall_springboot/src/main/java/com/rainypeople/tmall/service/CategoryService.java
+++ tmall_springboot/src/main/java/com/rainypeople/tmall/service/CategoryService.java
@@ -1,7 +1,7 @@
 package com.rainypeople.tmall.service;
 
-import com.rainypeople.tmall.pojo.Category;
 import com.rainypeople.tmall.dao.CategoryDao;
+import com.rainypeople.tmall.pojo.Category;
 import com.rainypeople.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,8 +9,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CategoryService {
